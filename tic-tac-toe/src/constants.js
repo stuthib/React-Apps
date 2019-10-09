a5a53bbed8dc81117f0a9e76e37ddff54b621b27
stuthib <stuthibalaji@gmail.com>
2019-10-09 09:54:43 -0400

    Delete constants.js

--- tic-tac-toe/src/constants.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-  REMAINING_TIME : 'REMAINING TIME IS ',
-  NEXT_PLAYER : 'NEXT PLAYER IS ',
-  WINNER : 'WINNER IS ',
-  NOT_DECIDED : 'NOT DECIDED',
-  GAME_OVER : 'GAME OVER!'
-};
-
-export default constants;
