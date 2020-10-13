eeebeadb17c016ad3c464e3a5f22bff70a01f951
stuthib <stuthibalaji@gmail.com>
2020-10-13 17:52:06 -0400

    adding google analytics

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Page from './components/Page/Page'
 import * as serviceWorker from './serviceWorker'
+import ReactGA from 'react-ga';
+
+const trackingId = "UA-163683236-2";
+ReactGA.initialize(trackingId);
 
 ReactDOM.render(
   <React.StrictMode>
